77726a661416afacb5367af4c71423bc1d324e53
Thiago Nascimento <tnascimento.thiago@gmail.com>
2026-02-03 22:57:56 +0000

    feat: nvme disk, efi, btrfs, zram swap

--- installation/setup_disk.sh
+++ installation/setup_disk.sh
@@ -3,10 +3,20 @@
 
 set -e
 
-# Partitioning
-parted -s /dev/sda mklabel msdos
-parted /dev/sda mkpart primary 1MiB 4GiB
-parted /dev/sda mkpart primary 4GiB 100%
+# Target Disk (use NVMe)
+DISK=/dev/nvme0n1
 
-mkswap /dev/sda1
-mkfs.ext4 /dev/sda2
+# Partitioning: create a GPT layout with an EFI System Partition and a btrfs root
+# - EFI partition: 1MiB..513MiB (~512MiB) (FAT32)
+# - Root partition: 513MiB..end (btrfs)
+# Note: this layout does not create a swap partition
+parted -s "$DISK" mklabel gpt
+parted -s "$DISK" mkpart ESP fat32 1MiB 513MiB # EFI ~512MiB
+parted "$DISK" set 1 boot on
+parted -s "$DISK" mkpart primary btrfs 513MiB 100%
+
+# Format EFI partition as FAT32
+mkfs.fat -F32 "${DISK}p1"
+
+# Format root partition as btrfs
+mkfs.btrfs -f "${DISK}p2"
